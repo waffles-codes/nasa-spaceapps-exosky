96bca310ec2727226d16ad1a4e02b94378ce5c45
waffles-codes <evalu802@gmail.com>
2024-10-04 23:11:41 -0700

    testing flask server

--- test.py
+++ test.py
@@ -2,6 +2,9 @@ import ssl
 from astroquery.gaia import Gaia
 import astropy.units as u
 from astropy.coordinates import SkyCoord
+from flask import Flask, jsonify, request
+import json
+app = Flask(__name__) #create a flask app
 
 try:
     _create_unverified_https_context = ssl._create_unverified_context
@@ -13,10 +16,18 @@ else:
     ssl._create_default_https_context = _create_unverified_https_context
 
 
-# Right ascension (RA) and declination (Dec) are celestial coordinates that specify the position of an object in the sky:
+# Right ascension (RA) and declination (Dec) are celestial coordinates that specify the position of an object in the sky.
 coord = SkyCoord(ra=280, dec=-60, unit=(u.degree, u.degree), frame='icrs')
 width = u.Quantity(0.1, u.deg)
 height = u.Quantity(0.1, u.deg)
 r = Gaia.query_object_async(coordinate=coord, width=width, height=height)
 
 r.pprint(max_lines=12, max_width=130)
+
+
+@app.route('/api/get_coords', methods=['GET'])
+def get_coords():
+    return jsonify(coord)
+
+if __name__ == '__main__':
+    app.run(port=5000)
